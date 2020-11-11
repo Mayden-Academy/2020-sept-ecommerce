3b7ed803d7985c5eb5f86eb650823606111242d2
Davin <davin.hayden@outlook.com>
2020-11-11 10:37:47 +0000

    changed namespace  from RobotStores to RobotStores\Hydrator and removed if statement for method getProductsByCharacter() thats part of story4 andadded \PDO

--- src/Hydrators/ProductHydrator.php
+++ src/Hydrators/ProductHydrator.php
@@ -1,30 +1,22 @@
 <?php
 
-
-namespace RobotStores;
-
+namespace RobotStores\Hydrators;
 
 class ProductHydrator
 {
     private array $productsArray;
 
     static public function getFilteredProducts(int $characterId = null):Array {
-        if ($characterId == null) {
-            $productsArray = self::getAllProducts();
-        }
-        else {
-            // run ProductHydrator getProductsByCharacter() method
-        }
+        $productsArray = self::getAllProducts();
         return $productsArray;
     }
 
-static private function getAllProducts():Array {
-    $dbConnection = DbConnector::getConnection();
-    $query = $dbConnection->prepare('SELECT `id`, `title`, `price`, `image` FROM `products`');
-    $query->execute();
-    $query->setFetchMode(PDO::FETCH_CLASS, 'BaseProduct');
-    return $query->fetchAll();
+    static private function getAllProducts():Array {
+        $dbConnection = \RobotStores\DbConnector::getConnection();
+        $query = $dbConnection->prepare('SELECT `id`, `title`, `price`, `image` FROM `products`');
+        $query->execute();
+        $query->setFetchMode(\PDO::FETCH_CLASS, 'RobotStores\Entities\BaseProduct');
+        return $query->fetchAll();
     }
-
 
 }
