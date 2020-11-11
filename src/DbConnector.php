cafc9625dd820211c9a9f74630c6eca601c8fd97
El Stevens <eleanorkwstevens@gmail.com>
2020-11-11 13:41:07 +0000

    made changes according to code review - added type hint and altered database name from robot-stores-dav to robot-stores

--- src/DbConnector.php
+++ src/DbConnector.php
@@ -1,16 +1,16 @@
 <?php
 
 namespace RobotStores;
-// FYI need '\PDO' in order to refer to global  namespace of PHP where PDO lives
+// FYI need '\PDO' in order to refer to global namespace of PHP where PDO lives
 
 class DbConnector
 {
-    static private $db;
+    static private \PDO $db;
 
     static public function getConnection(): \PDO
     {
         if ( self::$db == null ) {
-            $db = new \PDO('mysql:host=db;dbname=robot-stores-dav', 'root', 'password');
+            $db = new \PDO('mysql:host=db;dbname=robot-stores', 'root', 'password');
         }
         $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
         return $db;
