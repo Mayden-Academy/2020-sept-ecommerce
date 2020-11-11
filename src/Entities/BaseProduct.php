a5709c5ff57ba66dd58f71b77cf2d98fd530107f
El Stevens <eleanorkwstevens@gmail.com>
2020-11-11 13:42:33 +0000

    made changes according to code review - dropped curly braces on methods onto next line to follow formatting standards

--- src/Entities/BaseProduct.php
+++ src/Entities/BaseProduct.php
@@ -9,19 +9,23 @@ class BaseProduct
     protected float $price;
     protected string $image;
 
-    public function getID():int {
+    public function getID(): int
+    {
         return $this->id;
     }
 
-    public function getTitle():  string {
+    public function getTitle(): string
+    {
         return $this->title;
     }
 
-    public function getPrice(): float {
+    public function getPrice(): float
+    {
         return $this->price;
     }
 
-    public function getImage(): string {
+    public function getImage(): string
+    {
         return $this->image;
     }
 }
