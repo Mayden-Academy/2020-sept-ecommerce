fbb3298d34e8d43f295d83ca7d11c0c1e3d15c70
Davin <davin.hayden@outlook.com>
2020-11-11 10:34:36 +0000

    changed syntax for pdo to be \PDO in order to refer to global  namespace of php

--- src/DbConnector.php
+++ src/DbConnector.php
@@ -1,20 +1,20 @@
 <?php
 
-
 namespace RobotStores;
-
+// FYI need '\PDO' in order to refer to global  namespace of PHP where PDO lives
 
 class DbConnector
 {
     static private $db;
 
-    static public function getConnection(): PDO
+    static public function getConnection(): \PDO
     {
         if ( self::$db == null ) {
-            $db = new PDO('mysql:host=db;dbname=robot-stores-dav', 'root', 'password');
+            $db = new \PDO('mysql:host=db;dbname=robot-stores-dav', 'root', 'password');
         }
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
         return $db;
     }
 
 }
+
