1d22f536eaa8b3a30c29b1ecdefc15eb24d5148a
Ellie <erobinson98@btinternet.com>
2020-11-11 10:34:24 +0000

    html and styling done and fonts added

--- index.php
+++ index.php
@@ -6,33 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tab Title</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link href="static/css/normalise.css" type="text/css" rel="stylesheet"> <!-- this makes sure all browsers start with the same style -->
+    <link href="static/css/normalise.css" type="text/css" rel="stylesheet">
     <link href="static/css/style.css" type="text/css" rel="stylesheet">
 
 </head>
 <body>
-    <div class="container-fluid robot-header">
-        <div class="sticky-top text-center">
-            <h1>Robot Stores</h1>
+    <div class="container-fluid robotHeader">
+        <div class="sticky-top text-center align-middle">
+            <h1 class="align-middle">Robot Stores</h1>
         </div>
     </div>
     <div class="container text-center">
         <div class="row">
         <?php
-        $string='';
         foreach(ProductHydrator::getFilteredProducts() as $product) {
-            $string .= '<div class="col-md-3 d-flex flex-column justify-content-around baseProduct">
+            echo '<div class="col-md-3 d-flex flex-column justify-content-around baseProduct">
                             <img src=" ' . $product->getImage() . '" alt="product image">
                             <h2>' . $product->getTitle() . '</h2>
                             <p>£' . $product->getPrice() . '</p>
                             <button type="button" class="btn btn-info btn-sm">View Item</button>
                          </div>';
         }
-        echo $string;
         ?>
         </div>
     </div>
-    <div class="container-fluid robot-footer">
+    <div class="container-fluid robotFooter">
         <div class="text-center">
             <h4>Moss Piglets 2020</h4>
         </div>
