3dbbcd644749601a07bf65ac8f404a3030f578ef
El Stevens <eleanorkwstevens@gmail.com>
2020-11-11 13:44:37 +0000

    made changes according to code review - altered type hints to have space before them and to be lowercase, and added semicolon to end of query selector statement

--- src/Hydrators/ProductHydrator.php
+++ src/Hydrators/ProductHydrator.php
@@ -6,14 +6,15 @@ class ProductHydrator
 {
     private array $productsArray;
 
-    static public function getFilteredProducts(int $characterId = null):Array {
-        $productsArray = self::getAllProducts();
-        return $productsArray;
+    static public function getFilteredProducts(): array
+    {
+        return self::getAllProducts();
     }
 
-    static private function getAllProducts():Array {
+    static private function getAllProducts(): array
+    {
         $dbConnection = \RobotStores\DbConnector::getConnection();
-        $query = $dbConnection->prepare('SELECT `id`, `title`, `price`, `image` FROM `products`');
+        $query = $dbConnection->prepare('SELECT `id`, `title`, `price`, `image` FROM `products`;');
         $query->execute();
         $query->setFetchMode(\PDO::FETCH_CLASS, 'RobotStores\Entities\BaseProduct');
         return $query->fetchAll();
